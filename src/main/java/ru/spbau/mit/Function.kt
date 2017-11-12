7c4e672679daf9f075b1e180e476449a7860419b
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2017-11-12 17:16:14 +0300

    Replaced all exceptions with custom.

--- src/main/java/ru/spbau/mit/Function.kt
+++ src/main/java/ru/spbau/mit/Function.kt
@@ -1,5 +1,6 @@
 package ru.spbau.mit
 
+import ru.spbau.mit.exceptions.InterpretationException
 import ru.spbau.mit.parser.FplParser
 
 class Function(
@@ -9,7 +10,9 @@ class Function(
         private val block: FplParser.BlockWithBracesContext
 ) {
     fun invoke(arguments: List<Int>): Int {
-        if (arguments.size != parameterNames.size) throw Exception("$name was called with invalid number of arguments")
+        if (arguments.size != parameterNames.size) {
+            throw InterpretationException("$name was called with invalid number of arguments")
+        }
         val runScope = Scope(initializationScope)
         parameterNames.forEachIndexed { index, name ->
             runScope.initializeVariable(name)
--- src/main/java/ru/spbau/mit/Scope.kt
+++ src/main/java/ru/spbau/mit/Scope.kt
@@ -1,5 +1,7 @@
 package ru.spbau.mit
 
+import ru.spbau.mit.exceptions.InterpretationException
+
 class Scope(private val parentScope: Scope? = null) {
     private val functions = HashMap<String, Function>()
     private val variables = HashMap<String, Int>()
@@ -7,29 +9,29 @@ class Scope(private val parentScope: Scope? = null) {
     fun getVariableValue(name: String): Int
             = variables[name]
             ?: parentScope?.getVariableValue(name)
-            ?: throw Exception("Variable $name isn't defined")
+            ?: throw InterpretationException("Variable $name isn't defined")
 
     fun initializeVariable(name: String) {
-        if (name in variables) throw Exception("Variable $name is already defined in the scope")
+        if (name in variables) throw InterpretationException("Variable $name is already defined in the scope")
         variables[name] = 0
     }
 
     fun setVariableValue(name: String, value: Int) {
         if (name !in variables) {
-            parentScope?.setVariableValue(name, value) ?: throw Exception("Variable $name isn't defined")
+            parentScope?.setVariableValue(name, value) ?: throw InterpretationException("Variable $name isn't defined")
         } else {
             variables[name] = value
         }
     }
 
     fun identifyFunction(name: String, function: Function) {
-        if (name in functions) throw Exception("Function $name is already defined in the scope")
+        if (name in functions) throw InterpretationException("Function $name is already defined in the scope")
         functions[name] = function
     }
 
     fun getFunction(name: String): Function
             = functions[name]
             ?: parentScope?.getFunction(name)
-            ?: throw Exception("Function $name isn't defined")
+            ?: throw InterpretationException("Function $name isn't defined")
 
 }
