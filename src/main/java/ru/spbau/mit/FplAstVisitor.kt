83774fa62416b78d09747d212563c10f9935c507
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2017-11-26 13:56:53 +0300

    Made overrides not-null to avoid checks.

--- src/main/java/ru/spbau/mit/FplAstVisitor.kt
+++ src/main/java/ru/spbau/mit/FplAstVisitor.kt
@@ -6,10 +6,10 @@ import ru.spbau.mit.parser.FplParser
 
 class FplAstVisitor(private var scope: Scope = Scope()) : FplBaseVisitor<Int?>() {
 
-    override fun visitBlock(ctx: FplParser.BlockContext?): Int? {
+    override fun visitBlock(ctx: FplParser.BlockContext): Int? {
         val upperScope = scope
         scope = Scope(scope)
-        ctx!!.statement().forEach {
+        ctx.statement().forEach {
             val result = visit(it)
             if (result != null) {
                 scope = upperScope
@@ -20,35 +20,35 @@ class FplAstVisitor(private var scope: Scope = Scope()) : FplBaseVisitor<Int?>()
         return null
     }
 
-    override fun visitFunction(ctx: FplParser.FunctionContext?): Int? {
-        val name = ctx!!.Identifier().text
+    override fun visitFunction(ctx: FplParser.FunctionContext): Int? {
+        val name = ctx.Identifier().text
         val parameterNames = ctx.parameterNames().Identifier().map { it.text }
         val block = ctx.blockWithBraces()
         scope.identifyFunction(name, Function(name, parameterNames, scope, block))
         return null
     }
 
-    override fun visitNamedFunctionCall(ctx: FplParser.NamedFunctionCallContext?): Int {
-        val name = ctx!!.Identifier().text
+    override fun visitNamedFunctionCall(ctx: FplParser.NamedFunctionCallContext): Int {
+        val name = ctx.Identifier().text
         val arguments = ctx.arguments().expression().map { visit(it)!! }
         return scope.getFunction(name).invoke(arguments)
     }
 
-    override fun visitPrintLnFunctionCall(ctx: FplParser.PrintLnFunctionCallContext?): Int? {
-        val arguments = ctx!!.arguments().expression().map { visit(it) }
+    override fun visitPrintLnFunctionCall(ctx: FplParser.PrintLnFunctionCallContext): Int? {
+        val arguments = ctx.arguments().expression().map { visit(it) }
         println(arguments.joinToString(" "))
         return null
     }
 
-    override fun visitVariable(ctx: FplParser.VariableContext?): Int? {
-        val name = ctx!!.Identifier().text
+    override fun visitVariable(ctx: FplParser.VariableContext): Int? {
+        val name = ctx.Identifier().text
         scope.initializeVariable(name)
         if (ctx.expression() != null) scope.setVariableValue(name, visit(ctx.expression())!!)
         return null
     }
 
-    override fun visitWhileStatement(ctx: FplParser.WhileStatementContext?): Int? {
-        val condition = ctx!!.expression()
+    override fun visitWhileStatement(ctx: FplParser.WhileStatementContext): Int? {
+        val condition = ctx.expression()
         while (visit(condition) != 0) {
             val result = visit(ctx.blockWithBraces())
             if (result != null) return result
@@ -56,8 +56,8 @@ class FplAstVisitor(private var scope: Scope = Scope()) : FplBaseVisitor<Int?>()
         return null
     }
 
-    override fun visitIfStatement(ctx: FplParser.IfStatementContext?): Int? {
-        val condition = ctx!!.expression()
+    override fun visitIfStatement(ctx: FplParser.IfStatementContext): Int? {
+        val condition = ctx.expression()
         if (visit(condition) != 0) {
             return visit(ctx.blockWithBraces(0))
         } else if (ctx.blockWithBraces().size >= 2) {
@@ -66,17 +66,17 @@ class FplAstVisitor(private var scope: Scope = Scope()) : FplBaseVisitor<Int?>()
         return null
     }
 
-    override fun visitAssignment(ctx: FplParser.AssignmentContext?): Int? {
-        scope.setVariableValue(ctx!!.Identifier().text, visit(ctx.expression())!!)
+    override fun visitAssignment(ctx: FplParser.AssignmentContext): Int? {
+        scope.setVariableValue(ctx.Identifier().text, visit(ctx.expression())!!)
         return null
     }
 
-    override fun visitReturnStatement(ctx: FplParser.ReturnStatementContext?): Int {
-        return visit(ctx!!.expression())!!
+    override fun visitReturnStatement(ctx: FplParser.ReturnStatementContext): Int {
+        return visit(ctx.expression())!!
     }
 
-    override fun visitBinaryExpression(ctx: FplParser.BinaryExpressionContext?): Int {
-        val first = visit(ctx!!.expression(0))!!
+    override fun visitBinaryExpression(ctx: FplParser.BinaryExpressionContext): Int {
+        val first = visit(ctx.expression(0))!!
         val op = ctx.op.text
         when (op) {
             "||" -> if (first != 0) return 1
@@ -101,16 +101,16 @@ class FplAstVisitor(private var scope: Scope = Scope()) : FplBaseVisitor<Int?>()
         }
     }
 
-    override fun visitUnaryMinusExpression(ctx: FplParser.UnaryMinusExpressionContext?): Int {
-        return -visit(ctx!!.expression())!!
+    override fun visitUnaryMinusExpression(ctx: FplParser.UnaryMinusExpressionContext): Int {
+        return -visit(ctx.expression())!!
     }
 
-    override fun visitIdentifierExpression(ctx: FplParser.IdentifierExpressionContext?): Int {
-        return scope.getVariableValue(ctx!!.text)
+    override fun visitIdentifierExpression(ctx: FplParser.IdentifierExpressionContext): Int {
+        return scope.getVariableValue(ctx.text)
     }
 
-    override fun visitBracesExpression(ctx: FplParser.BracesExpressionContext?): Int {
-        return visit(ctx!!.expression())!!
+    override fun visitBracesExpression(ctx: FplParser.BracesExpressionContext): Int {
+        return visit(ctx.expression())!!
     }
 
     override fun visitLiteralExpression(ctx: FplParser.LiteralExpressionContext?): Int {
